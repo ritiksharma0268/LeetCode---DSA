ef61512f84574babcd27bc6f335af34876e16f21
Ritik Sharma <ritiksharma0268@gmail.com>
2026-07-13 20:44:22 +0530

    Time: 13 ms (6.67%), Space: 47.1 MB (13.9%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -2,30 +2,92 @@ class Solution
 {
     public String reverseVowels(String s) 
     {
+        //now using the two pointer approach //
         char alpha[] = s.toCharArray();
 
-        List<Character> vowels = new ArrayList<>();
+        int left = 0;
 
-        for(int i = 0;i<alpha.length;i++)
+        int right = alpha.length - 1;
+
+        List<Character> alphabet = new ArrayList<>();
+
+        alphabet.add('a');alphabet.add('e');alphabet.add('i');alphabet.add('o');alphabet.add('u');
+        alphabet.add('A');alphabet.add('E');alphabet.add('I');alphabet.add('O');alphabet.add('U');
+
+        while(left<=right)
         {
-            if(alpha[i] == 'a' || alpha[i] == 'e' || alpha[i] == 'i' || alpha[i] == 'o' || alpha[i] == 'u' || alpha[i] == 'A' || alpha[i] == 'E' || alpha[i] == 'I' || alpha[i] == 'O' || alpha[i] == 'U')
+            if(alphabet.contains(alpha[left]) && alphabet.contains(alpha[right]))
             {
-                vowels.add(alpha[i]);
+                char temp = alpha[left];
+                alpha[left] = alpha[right];
+                alpha[right] = temp;
+                left++;
+                right--;
             }
-        }
-        int k = 0;
-
-        for(int i = alpha.length-1;i>=0;i--)
-        {
-            if(alpha[i] == 'a' || alpha[i] == 'e' || alpha[i] == 'i' || alpha[i] == 'o' || alpha[i] == 'u' || alpha[i] == 'A' || alpha[i] == 'E' || alpha[i] == 'I' || alpha[i] == 'O' || alpha[i] == 'U')
+            else if(!alphabet.contains(alpha[left]))
             {
-                alpha[i] = vowels.get(k);
-                k++;
+                left++;
+            }
+            else if(!alphabet.contains(alpha[right]))
+            {
+                right--;
             }
         }
 
         String str2 = new String(alpha);
 
         return str2;
+           
     }
 }
+
+
+
+
+// // this is the brute force approach //
+//         char alpha[] = s.toCharArray();
+
+//         List<Character> vowels = new ArrayList<>();
+
+//         for(int i = 0;i<alpha.length;i++)
+//         {
+//             if(alpha[i] == 'a' || alpha[i] == 'e' || alpha[i] == 'i' || alpha[i] == 'o' || alpha[i] == 'u' || alpha[i] == 'A' || alpha[i] == 'E' || alpha[i] == 'I' || alpha[i] == 'O' || alpha[i] == 'U')
+//             {
+//                 vowels.add(alpha[i]);
+//             }
+//         }
+//         int k = 0;
+
+//         for(int i = alpha.length-1;i>=0;i--)
+//         {
+//             if(alpha[i] == 'a' || alpha[i] == 'e' || alpha[i] == 'i' || alpha[i] == 'o' || alpha[i] == 'u' || alpha[i] == 'A' || alpha[i] == 'E' || alpha[i] == 'I' || alpha[i] == 'O' || alpha[i] == 'U')
+//             {
+//                 alpha[i] = vowels.get(k);
+//                 k++;
+//             }
+//         }
+
+//         String str2 = new String(alpha);
+
+//         return str2;
+
+
+
+
+
+// if(alpha[left] == 'a' || alpha[left] == 'e' || alpha[left] == 'i' || alpha[left] == 'o' || alpha[left] == 'u' || alpha[left] == 'A' || alpha[left] == 'E' || alpha[left] == 'I' || alpha[left] == 'O' || alpha[left] == 'U' && alpha[right] == 'a' || alpha[right] == 'e' || alpha[right] == 'i' || alpha[right] == 'o' || alpha[right] == 'u' || alpha[right] == 'A' || alpha[right] == 'E' || alpha[right] == 'I' || alpha[right] == 'O' || alpha[right] == 'U')
+//             {
+//                 char temp = alpha[left];
+//                 alpha[left] = alpha[right];
+//                 alpha[right] = temp;
+//                 left++;
+//                 right--;
+//             }
+//             else if(alpha[left] != 'a' || alpha[left] != 'e' || alpha[left] != 'i' || alpha[left] != 'o' || alpha[left] != 'u' || alpha[left] != 'A' || alpha[left] != 'E' || alpha[left] != 'I' || alpha[left] != 'O' || alpha[left] != 'U')
+//             {
+//                 left++;
+//             }
+//             else if(alpha[right] != 'a' || alpha[right] != 'e' || alpha[right] != 'i' || alpha[right] != 'o' || alpha[right] != 'u' || alpha[right] != 'A' || alpha[right] != 'E' || alpha[right] != 'I' || alpha[right] != 'O' || alpha[right] != 'U')
+//             {
+//                 right--;
+//             }
