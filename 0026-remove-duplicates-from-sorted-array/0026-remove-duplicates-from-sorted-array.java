13940aedc76a382b64716b7617a1b2ee8338562c
Ritik Sharma <ritiksharma0268@gmail.com>
2026-07-18 14:08:23 +0530

    Time: 1 ms (78.76%), Space: 46.8 MB (24.81%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -2,29 +2,48 @@ class Solution
 {
     public int removeDuplicates(int[] nums) 
     {
-        Set<Integer> set1 = new HashSet<>();
+        // classic example of two pointers
 
-        Queue<Integer> q1 = new LinkedList<>();
+        int i = 1;
 
-        for(int i = 0;i<nums.length;i++)
+        for(int j = 1;j<nums.length;j++)
         {
-            if(set1.contains(nums[i]))
+            if(nums[j] != nums[i-1])
             {
-                continue;
-            }
-            else
-            {
-                set1.add(nums[i]);
-                q1.offer(nums[i]);
+                nums[i] = nums[j];
+                i++;
             }
         }
-
-        for(int i = 0;i<set1.size();i++)
-        {
-            nums[i] = q1.poll();
-        }
-
-
-        return set1.size();
+        return i;
     }
 }
+
+
+
+
+
+//  // this is using queue data set //
+//         Set<Integer> set1 = new HashSet<>();
+
+//         Queue<Integer> q1 = new LinkedList<>();
+
+//         for(int i = 0;i<nums.length;i++)
+//         {
+//             if(set1.contains(nums[i]))
+//             {
+//                 continue;
+//             }
+//             else
+//             {
+//                 set1.add(nums[i]);
+//                 q1.offer(nums[i]);
+//             }
+//         }
+
+//         for(int i = 0;i<set1.size();i++)
+//         {
+//             nums[i] = q1.poll();
+//         }
+
+
+//         return set1.size();
