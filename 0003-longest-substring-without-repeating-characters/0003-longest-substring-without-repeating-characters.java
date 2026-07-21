1a0c36c8cc9f37caf66c413cb4a3d95ba91c2ef9
Ritik Sharma <ritiksharma0268@gmail.com>
2026-07-21 18:51:03 +0530

    Time: 6 ms (70.47%), Space: 45.8 MB (63.43%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -2,73 +2,85 @@ class Solution
 {
     public int lengthOfLongestSubstring(String s) 
     {
-        char alpha[] = s.toCharArray();   
-        
-        int max = 0;
+        // now let us do using the two pointer or sliding window approach  //
+        Map<Character,Integer> map1 = new HashMap<>();
 
-        int len = alpha.length ;
+        int len = 0;
+        int maxlen = 0;
 
-        if(len>100)
+        int left = 0;
+        int right = 0;
+
+        while(right < s.length())
         {
-            len = 100;
-        }
-        
-        for(int i = 0;i<alpha.length;i++)
-        {
-            List<Character> list1 = new ArrayList<>();
-            int count = 0;
-
-            for(int j = i;j<len;j++)
+            if(map1.containsKey(s.charAt(right)) && map1.get(s.charAt(right)) >= left)
             {
-                if(list1.contains(alpha[j]))
-                {
-                    break;
-                }
-                list1.add(alpha[j]);
-                count++;
+                left = map1.get(s.charAt(right)) + 1;
+                
             }
-            if(count>max)
-            {
-                max = count;
-            }
+            
+                len = right - left + 1;
+                maxlen = Math.max(len,maxlen);
+                map1.put(s.charAt(right),right);
+                right++;
+            
         }
 
-        return max;
+        return maxlen;
 
+      
+
+       
     }
 }
 
 
 
 
-    //     // this is the most brute approach near about O(n^2);
-    //     if(s.length() == 1)
-    //     {
-    //         return 1;
-    //     }
 
-    //    char alpha[] = s.toCharArray();
-    //    int n = alpha.length;
-    //    int max = 0;
 
-    //    for(int i = 0;i<n;i++)
-    //    {
-    //         List<Character> list1 = new ArrayList<>();
-    //         int count = 0;
 
-    //         for(int j = i;j<n;j++)
-    //         {
-    //             if(list1.contains(alpha[j]))
-    //             {
-    //                 break;
-    //             }
-    //             list1.add(alpha[j]);
-    //             count++;
-    //         }
-    //         if(count>max)
-    //         {
-    //             max = count;
-    //         }
-    //    }
 
-    //    return max;
+
+
+// this is the most brute appraoch the key point was to note that we had only 100 unique characters //
+
+
+//    char alpha[] = s.toCharArray();   
+        
+//         int max = 0;
+
+//         int len = alpha.length ;
+
+//         if(len>100)
+//         {
+//             len = 100;
+//         }
+        
+//         for(int i = 0;i<alpha.length;i++)
+//         {
+//             List<Character> list1 = new ArrayList<>();
+//             int count = 0;
+
+//             for(int j = i;j<len;j++)
+//             {
+//                 if(list1.contains(alpha[j]))
+//                 {
+//                     break;
+//                 }
+//                 list1.add(alpha[j]);
+//                 count++;
+//             }
+//             if(count>max)
+//             {
+//                 max = count;
+//             }
+//         }
+
+//         return max;
+
+
+
+
+
+   
