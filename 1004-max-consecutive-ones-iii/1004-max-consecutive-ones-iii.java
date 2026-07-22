2581ca441f1d50101b6d440ce6adfd282798ef86
Ritik Sharma <ritiksharma0268@gmail.com>
2026-07-22 10:39:00 +0530

    Time: 4 ms (44.83%), Space: 52 MB (74.23%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -1,36 +1,72 @@
+// just reframe the question of longest subarray with atmost k zeroes //
+
 class Solution 
 {
     public int longestOnes(int[] nums, int k) 
     {
-        int n = nums.length ;
+        int n = nums.length;
 
         int left = 0;
-        int right  = 0;
+        int right = 0;
+        int zeros = 0;
 
-        int zero  = 0;
-
-        int maxlen  = 0;
-
+        int maxlen = 0;
 
         while(right < n)
         {
             if(nums[right] == 0)
             {
-                zero++;
+                zeros++;
             }
-            if(zero > k)
+            if(zeros>k)
             {
-                while(nums[left] != 0)
+                if(nums[left] == 0)
                 {
-                    left++;
-                }
-                left++;
-                zero--;
+                    zeros--;
+                }   
+                left++;   
             }
-            maxlen = Math.max(maxlen,right-left+1);
+            if(zeros<=k)
+            {
+                maxlen = Math.max(maxlen,right-left+1);
+            }
             right++;
         }
-
         return maxlen;
     }
 }
+
+
+
+//  // this is the better approach //
+//         // we are doing using the two pointer and sliding window appraoch  //
+//         int n = nums.length ;
+
+//         int left = 0;
+//         int right  = 0;
+
+//         int zero  = 0;
+
+//         int maxlen  = 0;
+
+
+//         while(right < n)
+//         {
+//             if(nums[right] == 0)
+//             {
+//                 zero++;
+//             }
+//             if(zero > k)
+//             {
+//                 while(nums[left] != 0)
+//                 {
+//                     left++;
+//                 }
+//                 left++;
+//                 zero--;
+//             }
+//             maxlen = Math.max(maxlen,right-left+1);
+//             right++;
+//         }
+
+//         return maxlen;
