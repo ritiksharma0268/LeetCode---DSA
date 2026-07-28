ea0de607c43337a9f127fa3be4fa8b8a8bf7fc12
Ritik Sharma <ritiksharma0268@gmail.com>
2026-07-28 12:49:10 +0530

    Time: 6 ms (100%), Space: 102.8 MB (5.19%) - LeetHub

--- 3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
+++ 3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
@@ -2,48 +2,81 @@ class Solution
 {
     public int minOperations(int[] nums) 
     {
-        int left = 0;
+        int n = nums.length;
 
-        int right = 0;
+        int ans = 0;
 
-        int ans  = 0;
-
-        while(right <  nums.length)
+        for(int i  = 0;i<=n-3;i++)
         {
-            if(nums[right] == 0)
+            if(nums[i] == 0)
             {
-                left = right ;
-                if(left<nums.length-2)
-                {
-                     for(int i = left;i<left+3;i++)
-                    {
-                        if(i < nums.length)
-                        {
-                            if(nums[i] == 0)
-                            {
-                                nums[i] = 1;
-                            }
-                            else if(nums[i] == 1)
-                            {
-                                nums[i] = 0;
-                            }
-                        }
-                    }
-                }
+                nums[i] = nums[i]^1;
+                nums[i+1] = nums[i+1]^1;
+                nums[i+2] = nums[i+2]^1;
 
                 ans++;
             }
-
-            right++;
         }
 
         if(nums[nums.length - 1] == 0 || nums[nums.length-2] == 0)
         {
             return -1;
         } 
-        
-      
+
         return ans;
+        
         
     }
 }
+
+
+
+
+
+
+        // // this is the sliding window approach by the moving the pointer by 3  every time we encounter 0//
+        
+        // int left = 0;
+
+        // int right = 0;
+
+        // int ans  = 0;
+
+        // while(right <  nums.length)
+        // {
+        //     if(nums[right] == 0)
+        //     {
+        //         left = right ;
+        //         if(left<nums.length-2)
+        //         {
+        //              for(int i = left;i<left+3;i++)
+        //             {
+        //                 if(i < nums.length)
+        //                 {
+        //                     if(nums[i] == 0)
+        //                     {
+        //                         nums[i] = 1;
+        //                     }
+        //                     else if(nums[i] == 1)
+        //                     {
+        //                         nums[i] = 0;
+        //                     }
+        //                 }
+        //             }
+        //         }
+
+        //         ans++;
+        //     }
+
+        //     right++;
+        // }
+        // // i had to check only the last two elements //
+
+
+        // if(nums[nums.length - 1] == 0 || nums[nums.length-2] == 0)
+        // {
+        //     return -1;
+        // } 
+        
+      
+        // return ans;
